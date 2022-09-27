95aca01239efcc13d88fa8f21a8532f78b1d855d
2389451262@qq.com <2389451262@qq.com>
2022-09-27 12:35:49 +0800

    fix(viewcontroller): implement ruler scroll.

--- src/pages/ViewController.vue
+++ src/pages/ViewController.vue
@@ -16,12 +16,12 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div class="view" ref="view">
-    <div class="view_ruler">
-      <RowRuler></RowRuler>
-      <ColumnRuler></ColumnRuler>
-    </div>
-    <div class="view_controller">
+  <div class="view">
+    <div class="view_controller" ref="view">
+      <div class="view_ruler">
+        <RowRuler></RowRuler>
+        <ColumnRuler></ColumnRuler>
+      </div>
       <div class="inner_controller">
         <ICanvas></ICanvas>
       </div>
@@ -32,25 +32,29 @@ onMounted(() => {
 <style scoped>
 .view {
   background-color: rgba(0, 0, 0, 0.2);
+  position: relative;
   overflow: auto;
 }
 .view_controller {
   width: 1920px;
   height: 1080px;
   padding: 66px;
+  box-sizing: content-box;
 }
 .inner_controller {
-  width: 100%;
-  height: 100%;
+  width: 1920px;
+  height: 1080px;
   background-color: #f0f0f0;
   border-radius: 4px;
   box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 20px 0px;
 }
 .view_ruler {
+  left: 0;
+  top: 0;
+  position: absolute;
   width: 100%;
   height: 100%;
-  position: fixed;
-  z-index: 1;
   pointer-events: none;
+  z-index: 3;
 }
 </style>
