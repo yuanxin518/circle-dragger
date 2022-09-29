d10699da1b1135a20b965336f0174e58f58e710d
2389451262@qq.com <2389451262@qq.com>
2022-09-30 00:04:48 +0800

    fix: collect component in a single file

--- src/pages/collectComponent.ts
+++ src/pages/collectComponent.ts
@@ -1,15 +1,15 @@
-import type { Component } from "vue";
+import { shallowReactive, type Component } from "vue";
 import * as DragElements from "../components/drag-elements/expose";
 
+export type DragComp = {
+  key: number;
+  component: Component;
+};
 // 引入所有内置的组件并渲染到CompController的容器中
-const compBucket = new Map();
-const defineDragComp = (
-  compName: string,
-  comp: {
-    key: number;
-    component: Component;
-  }
-) => {
+const compBucket = new Map<string, DragComp>();
+export const renderedComponents = shallowReactive<any[]>([] as DragComp[]);
+
+const defineDragComp = (compName: string, comp: DragComp) => {
   if (compBucket.has(compName)) {
     throw new Error(`there also exists a component named` + compName);
   }
@@ -22,5 +22,14 @@ for (const item of Object.entries(DragElements)) {
     component: item[1],
   });
 }
+
+export const renderComponent = (compName: string) => {
+  const dragComp = compBucket.get(compName);
+
+  if (dragComp) {
+    renderedComponents.push(dragComp);
+  }
+  console.log(renderedComponents);
+};
 
 export { compBucket };
